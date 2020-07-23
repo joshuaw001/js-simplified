0a81bcf960d7966a4d875bd899c7d302243ba33c
joshuaw001 <47309531+joshuaw001@users.noreply.github.com>
2020-07-23 12:02:18 -0500

    Update js_simp_v1.1.js

--- js_simp_v1.1.js
+++ js_simp_v1.1.js
@@ -1,1 +1,18 @@
-//qwerty
+//ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+-*/
+function $_(mode,args) {
+    const this.MODE_SELECT_TAG                 = 1.1
+    const this.MODE_SELECT_ID                  = 1.2
+    const this.MODE_SELECT_CLASS               = 1.3
+    const this.MODE_SELECT_ALTERNATE_ATTRIBUTE = 1.4
+    const this.MODE_SELECT_FROM_NODE_VARIABLE  = 1.5
+    
+    const this.MODE_DRAW_SHAPE                 = 2.0
+    const this.MODE_DRAW_SHAPE_TRIANGLE        = 2.1
+    const this.MODE_DRAW_SHAPE_SQUARE          = 2.2
+    const this.MODE_DRAW_SHAPE_RECTANGLE       = 2.3
+    const this.MODE_DRAW_SHAPE_PENTAGON        = 2.4
+    const this.MODE_DRAW_SHAPE_HEXAGON         = 2.5
+    switch(mode){
+        case 
+    }
+}
